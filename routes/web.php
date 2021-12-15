f860c17efc15721d00493249a487ca8117fb66b7
julio <julioakzzo@hotmail.com>
2021-12-15 16:07:58 -0300

    route add

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminEditServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
+use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
 use App\Http\Livewire\Admin\AdminServicesComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -48,5 +49,6 @@ Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function () 
     Route::get('/admin/service-categories', AdminServiceCategoryComponent::class)->name('admin.service_categories');
     Route::get('/admin/service-category/add', AdminAddServiceCategoryComponent::class)->name('admin.add_service_category');
     Route::get('/admin/service-category/edit/{category_id}', AdminEditServiceCategoryComponent::class)->name('admin.category.edit');
-    Route::get('/admin/all-services',AdminServicesComponent::class)->name('admin.all_services');
+    Route::get('/admin/all-services', AdminServicesComponent::class)->name('admin.all_services');
+    Route::get('/admin/{category_slug}/services', AdminServicesByCategoryComponent::class)->name('admin.services_by_category');
 });
